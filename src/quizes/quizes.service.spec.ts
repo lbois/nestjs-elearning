7107121153253006c26825d82cc4345542f9e403
Laurent Bois <laurent@air-de-laurent.lan>
2020-04-05 16:15:22 +0200

    Add a Jest test

--- src/quizes/quizes.service.spec.ts
+++ src/quizes/quizes.service.spec.ts
@@ -5,44 +5,94 @@ import { QuizesService } from './quizes.service';
 import { usersProviders } from '../auth/users.providers';
 import { Test } from '@nestjs/testing';
 import * as mongoose from 'mongoose';
-import mockingoose from 'mockingoose';
 import { User } from 'src/auth/user.interface';
 import { Quiz } from './quiz.interface';
+import { databaseProviders } from '../database.providers';
+import { getModelToken } from '@nestjs/mongoose';
 
-const USER_MODEL =  mongoose.model('User', UserSchema);
+const userModel:mongoose.Model<User> =  mongoose.model('User', UserSchema);
 
-const CLASS_MODEL =  mongoose.model('Quiz', QuizSchema);
+const CLASS_MODEL:mongoose.Model<Quiz> =  mongoose.model('Quiz', QuizSchema);
 
-const mockingQuizModel = () => {
-
+function mockingQuizModel()  {
+  this.find = jest.fn()
 }
 
-const mockingUserModel = () => {
 
+
+
+   function mockingUserModel() {
+    this.find = jest.fn()
+   }
+   
+
+const mockUser = {
+  username: 'Test user'
 }
 
 describe('QuizesService', () => {
     let quizesService;
-    let userModel, classModel;
+    let clientUserModel , clientClassModel;
     
 
     beforeEach(async () => {
+        
         const module = await Test.createTestingModule({
-            providers: [QuizesService,
-            {provide: USER_MODEL, useFactory: mockingUserModel},
-            {provide: CLASS_MODEL, useFactory: mockingQuizModel},
+            
+            providers: [QuizesService, //...usersProviders, ...quizesProviders,...databaseProviders,
+            {provide: getModelToken('USER_MODEL'), useValue: new mockingUserModel()},
+            {provide: getModelToken('CLASS_MODEL'), useValue: new mockingQuizModel()},
+           // {provide: 'DATABASE_CONNECTION', useFactory: mockingDatabaseConnection},
         ],
         }).compile();
 
-        quizesService = await module.get<QuizesService>(QuizesService);
-        classModel = await module.get<mongoose.Model<Quiz>>(CLASS_MODEL)
-        userModel = await module.get<mongoose.Model<User>>(USER_MODEL)
+        quizesService = module.get<QuizesService>(QuizesService);
+        clientClassModel = module.get(getModelToken('CLASS_MODEL'))
+        clientUserModel =  module.get(getModelToken('USER_MODEL'))
+        //dbConnection = await module.get<mongoose.Connection>('DATABASE_CONNECTION')
+        //console.log(classModel);
+        // console.log(clientClassModel);
+        
     })
 
     describe('getAllQuizes', ()=> {
 
-        it('get all quizes', () => {
-           // expect(quizesRepository.getAllQuizes).not.toHaveBeenCalled();
+        it('get all quizes, user not found', async () => {
+            clientUserModel.find.mockRejectedValue('user not found');
+            clientClassModel.find.mockResolvedValue([{title: 'test', description: 'test'}])
+            
+           //jest.spyOn(clientUserModel, 'find');
+           // jest.spyOn(clientClassModel, 'find');
+           expect(clientUserModel.find).not.toHaveBeenCalled();
+           expect(clientClassModel.find).not.toHaveBeenCalled();
+           const result = quizesService.getAllQuizes(mockUser).catch((err)=>{
+            expect(err.message).toEqual('user not found');
+           });
+           expect(clientUserModel.find).toHaveBeenCalled();
+
+           // expect(clientClassModel.find).toHaveBeenCalled();
+           
+           
+        })
+
+        it('get all quizes, find quizzes', async () => {
+            clientUserModel.find.mockReturnValue({_id:'1234', username:'Test user'});
+            clientClassModel.find.mockResolvedValue([{title: 'test', description: 'test'}])
+            
+           //jest.spyOn(clientUserModel, 'find');
+           // jest.spyOn(clientClassModel, 'find');
+           expect(clientUserModel.find).not.toHaveBeenCalled();
+           expect(clientClassModel.find).not.toHaveBeenCalled();
+           const result =  quizesService.getAllQuizes(mockUser).then(state=> {
+            expect(clientUserModel.find).toHaveBeenCalled();
+            expect(clientClassModel.find).toHaveBeenCalled();
+            expect(state).toEqual([{title: 'test', description: 'test'}])
+           });
+           
+
+           // 
+           
+           
         })
 
     })
