5e457ddccb811dd5460d679dda102bb7a1e9017c
Laurent Bois <laurent@air-de-laurent.lan>
2020-04-30 12:38:35 +0200

    modified port with env var

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,7 +10,7 @@ import { join } from 'path';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://heroku_2sx94z36:6bs6pca3be7vjrdt0kpeo5bppj@ds027489.mlab.com:27489/heroku_2sx94z36'),//mongodb://localhost/quiz-challenge'),
+    MongooseModule.forRoot('mongodb://localhost/quiz-challenge'),
     QuizesModule, AuthModule, QuestionsModule, AnswersModule,
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'client'),
