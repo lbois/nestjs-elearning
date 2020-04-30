52b54748666cfe9fad6e55e8d82d7fa962786c01
Laurent Bois <laurent@air-de-laurent.lan>
2020-04-30 06:54:43 +0200

    Heroku changes

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+let port = process.env.PORT || 3000
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {cors: true});
-  await app.listen(3000);
+  await app.listen(port);
 }
 bootstrap();
